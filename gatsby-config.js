9702d051fcc924d7c63375d150341ba52f1faf1d
Bhashkar Sharma <bhashkarsharma@gmail.com>
2018-06-03 22:19:05 +0200

    We don't need no sourcemaps

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-no-sourcemaps`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`
