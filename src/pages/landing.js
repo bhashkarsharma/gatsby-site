6915b22e3bf0b97490b9f707b6cd1be34d360f04
Bhashkar Sharma <bhashkarsharma@gmail.com>
2018-06-07 07:58:55 +0200

    Moving images around

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
 import BannerLanding from '../components/BannerLanding'
-
-import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
-import pic10 from '../assets/images/pic10.jpg'
+import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
+import React from 'react'
+import pic08 from '../assets/images/clicks/pic08.jpg'
+import pic09 from '../assets/images/clicks/pic09.jpg'
+import pic10 from '../assets/images/clicks/pic10.jpg'
 
 const Landing = (props) => (
     <div>
